5a37343bceef9811f4bfd572d98ec4066d8e6ef7
Emil <emilbabazade29@gmail.com>
2025-01-10 22:45:32 +0200

    transition code refactoring

--- scenes/user interface/transition_layer.gd
+++ scenes/user interface/transition_layer.gd
@@ -6,6 +6,6 @@ var transparent = Color(0, 0, 0, 0)
 func change_scene(target_scene: StringName):
 	var tween = create_tween()
 	tween.tween_property($ColorRect, 'color', black, 1)
-	await tween.finished
+	tween.tween_property($ColorRect, 'color', transparent, 1)
+	await tween.step_finished
 	get_tree().change_scene_to_file(target_scene)
-	create_tween().tween_property($ColorRect, 'color', transparent, 1)
