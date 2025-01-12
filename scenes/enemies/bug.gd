61385cf17111f3c01f72c158a2aca4dfe6ebdb14
Emil <emilbabazade29@gmail.com>
2025-01-12 21:49:46 +0200

    bug attack

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 var is_player_detected = false
+var is_player_in_attack_range = false
+var attack_enabled = true
 @onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 var speed = 200
 
-func _process(delta):
-	if is_player_detected:
+func _process(_delta):
+	if is_player_in_attack_range and attack_enabled:
+		attack()
+	elif is_player_detected:
 		walk_to_player()
 
 func walk_to_player():
@@ -15,9 +19,28 @@ func walk_to_player():
 	velocity = speed * direction
 	move_and_slide()
 
+func attack():
+	animator.play('attack')
+	await animator.animation_finished
+	Globals.health -= 10
+	attack_enabled = false
+	$AttackCooldown.start()
+
 func _on_notice_area_2d_body_entered(_body):
 	is_player_detected = true
 
 
 func _on_notice_area_2d_body_exited(_body):
 	is_player_detected = false
+
+
+func _on_attack_area_2d_body_entered(_body):
+	is_player_in_attack_range = true
+
+
+func _on_attack_area_2d_body_exited(_body):
+	is_player_in_attack_range = false
+
+
+func _on_attack_cooldown_timeout():
+	attack_enabled = true
