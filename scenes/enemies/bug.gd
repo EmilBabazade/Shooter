edb20e1c70b77c80baab15b1366f7fb3d853265c
Emil <emilbabazade29@gmail.com>
2025-01-12 22:00:57 +0200

    bug dmaage

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -3,8 +3,10 @@ extends CharacterBody2D
 var is_player_detected = false
 var is_player_in_attack_range = false
 var attack_enabled = true
+var invulnerable = false
 @onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 var speed = 200
+var health = 50
 
 func _process(_delta):
 	if is_player_in_attack_range and attack_enabled:
@@ -44,3 +46,18 @@ func _on_attack_area_2d_body_exited(_body):
 
 func _on_attack_cooldown_timeout():
 	attack_enabled = true
+
+func hit(damage):
+	if invulnerable:
+		return
+	health -= damage
+	animator.material.set_shader_parameter('progress', 1)
+	if health <= 0:
+		queue_free()
+	invulnerable = true
+	$InvulnerableTimer.start()
+
+
+func _on_invulnerable_timer_timeout():
+	animator.material.set_shader_parameter('progress', 0)
+	invulnerable = false
