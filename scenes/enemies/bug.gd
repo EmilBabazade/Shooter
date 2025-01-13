998fb67e9c42915510bf6d24802019fde867df92
Emil <emilbabazade29@gmail.com>
2025-01-13 20:01:11 +0200

    bug enemy attack bug fixed

--- scenes/enemies/bug.gd
+++ scenes/enemies/bug.gd
@@ -2,16 +2,18 @@ extends CharacterBody2D
 
 var is_player_detected = false
 var is_player_in_attack_range = false
-var attack_enabled = true
+#var attack_enabled = true
 var invulnerable = false
 @onready var animator: AnimatedSprite2D = $AnimatedSprite2D
 var speed = 200
 var health = 50
 
 func _process(_delta):
-	if is_player_in_attack_range and attack_enabled:
-		attack()
-	elif is_player_detected:
+	#if is_player_in_attack_range and attack_enabled:
+		#attack()
+	#elif is_player_detected and !is_player_in_attack_range:
+		#walk_to_player()
+	if is_player_detected:
 		walk_to_player()
 
 func walk_to_player():
@@ -21,12 +23,12 @@ func walk_to_player():
 	velocity = speed * direction
 	move_and_slide()
 
-func attack():
-	animator.play('attack')
-	await animator.animation_finished
-	Globals.health -= 10
-	attack_enabled = false
-	$AttackCooldown.start()
+#func attack():
+	#animator.play('attack')
+	##await animator.animation_finished
+	#Globals.health -= 10
+	#attack_enabled = false
+	#$AttackCooldown.start()
 
 func _on_notice_area_2d_body_entered(_body):
 	is_player_detected = true
@@ -34,10 +36,13 @@ func _on_notice_area_2d_body_entered(_body):
 
 func _on_notice_area_2d_body_exited(_body):
 	is_player_detected = false
+	animator.stop()
 
 
 func _on_attack_area_2d_body_entered(_body):
 	is_player_in_attack_range = true
+	is_player_detected = false
+	animator.play('attack')
 
 
 func _on_attack_area_2d_body_exited(_body):
@@ -45,7 +50,7 @@ func _on_attack_area_2d_body_exited(_body):
 
 
 func _on_attack_cooldown_timeout():
-	attack_enabled = true
+	animator.play('attack')
 
 func hit(damage):
 	if invulnerable:
@@ -61,3 +66,11 @@ func hit(damage):
 func _on_invulnerable_timer_timeout():
 	animator.material.set_shader_parameter('progress', 0)
 	invulnerable = false
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if is_player_in_attack_range:
+		Globals.health -= 10
+		$AttackCooldown.start()
+	else:
+		is_player_detected = true
